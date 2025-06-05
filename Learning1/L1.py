5af011741832aed9bc1e12362ab440385c85d03f
khushbu modi <khushbu.modi@ispl.com>
2025-06-05 19:00:33 +0530

    print function

---
+++ Learning1/L1.py
@@ -0,0 +1,21 @@
+# print("Hello", 3.14, "Khushbu", sep="--")
+# name = input("Enter your name: ")
+# print("Your name is:",name)
+# age= 65
+# pi = 3.15
+# first_name= "khushbu"
+# print(type(age))
+# print(type(pi))
+# print(type(first_name))
+
+i1 = int(input("Enter one number: "))
+i2 = int(input("Enter second number: "))
+print("Max number is: ", max(i1,i2))
+print(type(i1))
+print("sum =", i1 + i2)
+print("Subtraction= ",i1 - i2)
+print("Multipication= ", i1 * i2)
+print("Division= ", i2/i1)
+
+
+
